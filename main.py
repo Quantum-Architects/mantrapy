43faa1d49001601893aecf161f29c0db1324be90
hanchon <guillermo.paoletti@gmail.com>
2024-10-25 19:35:30 +0400

    wallet generator

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # from mantrapy.txbuilder.builder import tx_builder
-from mantrapy.wallet.wallet import new_wallet
+from mantrapy.wallet.wallet import test_wallet
 
 # t = tx_builder("")
 # t.bank_send("",1,"")
-w = new_wallet()
-print(w)
+w = test_wallet()
+print(w.address)
 
