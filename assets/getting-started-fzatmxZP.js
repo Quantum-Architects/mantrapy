a9a8f427965283916320626a3affa94f49a779e3
hanchon <hanchon@users.noreply.github.com>
2024-10-26 12:14:49 +0000

    deploy: 44878276116cb41d5ba221ab90a9ee5902c7c0d6

--- assets/getting-started-fzatmxZP.js
+++ assets/getting-started-fzatmxZP.js
@@ -1,10 +1,11 @@
-import{u as i,j as e}from"./index-BVAeCDkq.js";const d={title:"Get started",description:"undefined"};function t(s){const n={a:"a",code:"code",div:"div",h1:"h1",header:"header",li:"li",p:"p",ul:"ul",...i(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"get-started",children:["Get started",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#get-started",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+import{u as i,j as e}from"./index-BBBqJsQO.js";const d={title:"Get started",description:"undefined"};function t(s){const n={a:"a",code:"code",div:"div",h1:"h1",header:"header",li:"li",p:"p",ul:"ul",...i(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"get-started",children:["Get started",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#get-started",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
 `,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"mantra-py"})," lib provides 4 main modules:"]}),`
 `,e.jsxs(n.ul,{children:[`
 `,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/wallet",children:e.jsx(n.code,{children:"Wallet"})}),", to create addresses and send transactions."]}),`
 `,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/client",children:e.jsx(n.code,{children:"Client"})}),", to query blockchain information."]}),`
 `,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/txbuilder",children:e.jsx(n.code,{children:"TxBuilder"})}),", to interact with the blockchain."]}),`
 `,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/webhooks",children:e.jsx(n.code,{children:"Webhooks"})}),", to subscribe to on-chain events."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/constants",children:e.jsx(n.code,{children:"Constants"})}),", to get the chain configuration values."]}),`
 `]}),`
 `,e.jsx(n.p,{children:"Examples"}),`
 `,e.jsxs(n.ul,{children:[`
---
+++ assets/index-DvPVn_9h.js
@@ -0,0 +1,19 @@
+import{u as a,j as n}from"./index-BBBqJsQO.js";const r={title:"Constants",description:"undefined"};function i(s){const e={a:"a",code:"code",div:"div",h1:"h1",h2:"h2",header:"header",li:"li",p:"p",pre:"pre",span:"span",ul:"ul",...a(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.header,{children:n.jsxs(e.h1,{id:"constants",children:["Constants",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#constants",children:n.jsx(e.div,{"data-autolink-icon":!0})})]})}),`
+`,n.jsxs(e.p,{children:["The ",n.jsx(e.code,{children:"constants"})," pkg is used to read from the environment variable the chain values."]}),`
+`,n.jsxs(e.h2,{id:"env-vars",children:["Env Vars",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#env-vars",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"'API_ENDPOINT'"}),`
+`,n.jsx(e.li,{children:"'RPC_ENDPOINT'"}),`
+`,n.jsx(e.li,{children:"'CHAIN_ID'"}),`
+`,n.jsx(e.li,{children:"'DENOM', the denomination is used to pay the network fees."}),`
+`]}),`
+`,n.jsxs(e.h2,{id:"default-values",children:["Default Values",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#default-values",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsxs(e.p,{children:["If the env vars are not set, the ",n.jsx(e.code,{children:"constants"})," object will default to Mantra Mainnet."]}),`
+`,n.jsxs(e.p,{children:["Calling the ",n.jsx(e.code,{children:"constants.testnet()"})," function will overwrite the values with the Testnet configuration."]}),`
+`,n.jsxs(e.h2,{id:"usage",children:["Usage",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#usage",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsx(e.pre,{className:"shiki shiki-themes github-light github-dark-dimmed",style:{backgroundColor:"#fff","--shiki-dark-bg":"#22272e",color:"#24292e","--shiki-dark":"#adbac7"},tabIndex:"0",children:n.jsxs(e.code,{children:[n.jsxs(e.span,{className:"line",children:[n.jsx(e.span,{style:{color:"#D73A49","--shiki-dark":"#F47067"},children:"from"}),n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:" mantrapy.constants.constants "}),n.jsx(e.span,{style:{color:"#D73A49","--shiki-dark":"#F47067"},children:"import"}),n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:" Constants"})]}),`
+`,n.jsx(e.span,{className:"line","data-empty-line":!0,children:" "}),`
+`,n.jsxs(e.span,{className:"line",children:[n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"c "}),n.jsx(e.span,{style:{color:"#D73A49","--shiki-dark":"#F47067"},children:"="}),n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:" Constants()"})]}),`
+`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"c.testnet()"})}),`
+`,n.jsxs(e.span,{className:"line",children:[n.jsx(e.span,{style:{color:"#005CC5","--shiki-dark":"#6CB6FF"},children:"print"}),n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"(c.api_endpoint)"})]}),`
+`,n.jsxs(e.span,{className:"line",children:[n.jsx(e.span,{style:{color:"#005CC5","--shiki-dark":"#6CB6FF"},children:"print"}),n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"(c.rpc)"})]})]})})]})}function l(s={}){const{wrapper:e}={...a(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(i,{...s})}):i(s)}export{l as default,r as frontmatter};
