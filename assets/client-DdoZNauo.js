69c263b0b77f7c8174507ac43bd3d3da5d15f352
hanchon <hanchon@users.noreply.github.com>
2024-10-26 12:18:33 +0000

    deploy: 837bbe92891f34909b710ed391f4bcc1e59f7687

---
+++ assets/client-DdoZNauo.js
@@ -0,0 +1,5 @@
+import{u as r,j as n}from"./index-CKKeU-6_.js";const d={title:"Client",description:"undefined"};function i(t){const e={a:"a",div:"div",h1:"h1",header:"header",li:"li",p:"p",ul:"ul",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.header,{children:n.jsxs(e.h1,{id:"client",children:["Client",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#client",children:n.jsx(e.div,{"data-autolink-icon":!0})})]})}),`
+`,n.jsx(e.p,{children:"Mantrapy provides a client to interact with Mantra network via:"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"API: to"}),`
+`]})]})}function o(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(i,{...t})}):i(t)}export{o as default,d as frontmatter};
