6e90c5005bf396fbb2dc21482e3c668f081957d7
stepit <48993133+0xstepit@users.noreply.github.com>
2024-10-26 08:41:06 +0200

    feat: Add delegations query (#7)
    * update examples client
    
    * add delegation query

--- examples/queries/balances.py
+++ examples/queries/balances.py
@@ -1,7 +1,7 @@
-from mantrapy.querier.querier import API, RPC, Querier
+from mantrapy.client.client import API, RPC, Client
 
-querier = Querier(API, RPC)
+client = Client(API, RPC)
 
-balances_resp = querier.get_balances("mantra1n4u9s9h3c670s7wsfycf6v7d7f2t55ql9gm3sj")
+balances_resp = client.get_balances("mantra1n4u9s9h3c670s7wsfycf6v7d7f2t55ql9gm3sj")
 print(balances_resp.data)
 print(balances_resp.status_code)
--- examples/queries/tx.py
+++ examples/queries/tx.py
@@ -1,9 +1,9 @@
-from mantrapy.querier.querier import API, RPC, Querier
+from mantrapy.client.client import API, RPC, Client
 
-querier = Querier(API, RPC)
+client = Client(API, RPC)
 
 
-tx_resp = querier.get_tx_by_hash(
+tx_resp = client.get_tx_by_hash(
     "0xC414DBB9C80503DFF20684536710FD4FC0401B39C25B0171C752ABCE3907CD95"
 )
 print(tx_resp)
