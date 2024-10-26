a7a4d126fea48d5fdc4422b3e08ea1ebbfbe1669
hanchon <hanchon@users.noreply.github.com>
2024-10-26 09:09:15 +0000

    deploy: cb21ef845526458c0ca7817f686210b6fd57c9e9

---
+++ assets/getting-started-DPWWrLzT.js
@@ -0,0 +1,12 @@
+import{u as i,j as e}from"./index-BFq3gebJ.js";const d={title:"Get started",description:"undefined"};function t(s){const n={a:"a",code:"code",div:"div",h1:"h1",header:"header",li:"li",p:"p",ul:"ul",...i(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"get-started",children:["Get started",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#get-started",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"mantra-py"})," lib provides 4 main modules:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/wallet",children:e.jsx(n.code,{children:"Wallet"})}),", to create addresses and send transactions."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/client",children:e.jsx(n.code,{children:"Client"})}),", to query blockchain information."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/txbuilder",children:e.jsx(n.code,{children:"TxBuilder"})}),", to interact with the blockchain."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/webhooks",children:e.jsx(n.code,{children:"Webhooks"})}),", to subscribe to on-chain events."]}),`
+`]}),`
+`,e.jsx(n.p,{children:"Examples"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"/webhooks-api",children:e.jsx(n.code,{children:"Webhooks API"})}),", using the ",e.jsx(n.code,{children:"Webhooks"})," module, it provides an API to subscribe to events from outside your program."]}),`
+`]})]})}function o(s={}){const{wrapper:n}={...i(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(t,{...s})}):t(s)}export{o as default,d as frontmatter};
